e6685dfdad42b5ba15043f1d6da90fd9f18c3b1b
wzc-OntarioTechU <william.chamberlain@ontariotechu.net>
2024-02-16 22:04:53 -0500

    Added Lab 1 Files,
    Binary.java replaced with lab1 version.
    BinaryTest.java added from lab1.

--- src/main/java/com/ontariotechu/sofe3980U/Binary.java
+++ src/main/java/com/ontariotechu/sofe3980U/Binary.java
@@ -29,12 +29,10 @@ public class Binary
 		}
 		//beg has the index of the first non zero digit in the number
 		this.number=number.substring(beg); // exclude the trailing zeros if any
-		// uncomment the following code
-		
+
 		if(this.number=="") { // replace empty strings with a single zero
 			this.number="0";
 		}
-		
     }
 	/**
 	* Return the binary value of the variable
@@ -78,5 +76,85 @@ public class Binary
 		Binary result=new Binary(num3);  // create a binary object with the calculated value.
 		return result;
 		
+	}
+	
+	/**
+	 * Bitwise OR two binary variables.
+	 * 
+	 * @param num1 The first input binary number.
+	 * @param num2 The second input binary number.
+	 * @return A binary variable with a value of <i>num1 OR num2</i>.
+	 */
+	public static Binary or(Binary num1, Binary num2) {
+		// gather the larger of the two index
+		int ind = num1.number.length() > num2.number.length() ? num1.number.length() : num2.number.length();
+		// string to hold output
+		StringBuilder num3 = new StringBuilder("");
+		// loop over all available digits
+		for (int i = 1; i <= ind; i++) {
+			// check for unequal length strings
+			if (num1.number.length() < i) { // first num is shorter, use second
+				num3.append(num2.number.charAt(num2.number.length() - i));
+			} else if (num2.number.length() < i) { // second num is shorter, use first
+				num3.append(num1.number.charAt(num1.number.length() - i));
+			} else { // both have a valid digit, compare
+				num3.append((num1.number.charAt(num1.number.length() - i) == '1' || num2.number.charAt(num2.number.length() - i) == '1') ? "1" : "0");
+			}
+		}
+		// reverse string builder since we appended instead of prepended
+		num3.reverse();
+		return new Binary(num3.toString());
+	}
+	
+	/**
+	 * Bitwise AND two binary variables.
+	 * 
+	 * @param num1 The first input binary number.
+	 * @param num2 The second input binary number.
+	 * @return A binary variable with a value of <i>num1 AND num2</i>
+	 */
+	public static Binary and(Binary num1, Binary num2) {
+		// gather the larger of the two index
+		int ind = num1.number.length() > num2.number.length() ? num1.number.length() : num2.number.length();
+		// string to hold output
+		StringBuilder num3 = new StringBuilder("");
+		// loop over all available digits
+		for (int i = 1; i <= ind; i++) {
+			// check for unequal length strings
+			if (num1.number.length() < i || num2.number.length() < i) { // if either num is too shorter
+				num3.append("0");
+			} else { // both have a valid digit, compare
+				num3.append((num1.number.charAt(num1.number.length() - i) == '1' && num2.number.charAt(num2.number.length() - i) == '1') ? "1" : "0");
+			}
+		}
+		// reverse string builder since we appended instead of prepended
+		num3.reverse();
+		return new Binary(num3.toString());
+	}
+	
+	/**
+	 * Multiply two binary numbers.
+	 * 
+	 * @param num1 The first binary multiplicand.
+	 * @param num2 The second binary multiplicand.
+	 * @return A binary variable with a value of <i>num1 * num2</i>
+	 */
+	public static Binary multiply(Binary num1, Binary num2) {
+		// approaching using shifting methodology
+		// assign shorter and longer of the multiplicands to new pointers to reduce loop cycles
+		Binary longer = num1.number.length() > num2.number.length() ? num1 : num2;
+		Binary shorter = num1.number.length() <= num2.number.length() ? num1 : num2;
+		// new empty binary object to collect result
+		Binary result = new Binary("0");
+		// loop over shorter
+		for (int i = shorter.number.length() - 1; i >= 0; i--) {
+			// check the appropriate character at index in shorter
+			if (shorter.number.charAt(i) == '1') { // 1 so add
+				result = Binary.add(result, longer);
+			}
+			// append 0 to longer to effectively bit shift
+			longer = new Binary(longer.number + "0");
+		}
+		return result;
 	}
 }	
